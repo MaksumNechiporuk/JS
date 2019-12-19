bbc1f3554343e4855cb1da92a4d4d9765f121ca4
MaksumNechiporuk <maksum.nec@gmail.com>
2019-12-19 10:19:00 +0200

    google maps

--- API/DOM/js/index.js
+++ API/DOM/js/index.js
@@ -1,59 +1,24 @@
-// let elem = document.getElementsByTagName("li"); //document - це обєкт для роботи з DOM деревом
-// console.log(elem);
-// for (let i = 0; i < elem.length; i++) {
-//   elem[i].innerHTML = `Element number ${i}`;
-// }
-
-// const elem = document.getElementById("root");
-// elem.innerHTML = "<h2>Hello World</h2>";
-
-// const elem = document.getElementsByClassName("root")[0];
-// elem.removeAttribute("id");
-// elem.setAttribute("class", "new--class");
-// tagA = document.createElement("a");
-
-// tagA.setAttribute("href", "#");
-// tagA.innerHTML = "Click";
-// elem.appendChild(tagA);
-
-// const elements1 = ["Firstname", "Lastname", "Age"];
-// const elements2 = ["Jill", "Smith", "50"];
-// const elements3 = ["Eve", "Jackson", "94"];
-// const elem = document.getElementById("root");
-// table = document.createElement("table");
-// row = document.createElement("tr");
-// for (let i = 0; i < elements1.length; i++) {
-//   th = document.createElement("th");
-//   th.innerHTML = elements1[i];
-//   row.appendChild(th);
-// }
-// table.appendChild(row);
-// row = document.createElement("tr");
-// for (let i = 0; i < elements2.length; i++) {
-//   td = document.createElement("td");
-//   td.innerHTML = elements2[i];
-//   row.appendChild(td);
-// }
-// table.appendChild(row);
-// row = document.createElement("tr");
-// for (let i = 0; i < elements3.length; i++) {
-//   td = document.createElement("td");
-//   td.innerHTML = elements3[i];
-//   row.appendChild(td);
-// }
-// table.appendChild(row);
-// row = document.createElement("tr");
-// elem.appendChild(table);
-
 window.addEventListener("load", Init);
 let select_first = document.createElement("select");
+let select_second = document.createElement("select");
 let inputMoney = document.createElement("input");
 let label = document.createElement("label");
+let rezBuy;
+let rezSale;
 inputMoney.addEventListener("change", Convert);
 select_first.addEventListener("change", Convert);
+select_first.addEventListener("change", FillSelect);
+select_second.addEventListener("change", Convert);
+function FillSelect() {
+  let currency_second = Array.from(select_first.options)
+    .filter(option => option.ccy != "BTC")
+    .map(currency_second => "UAH");
+  select_second.options = currency_second;
+}
 function Convert() {
   let money = inputMoney.value;
   let currency1 = select_first.value;
+  let currency2 = select_second.value;
   let courseBuy = parseFloat(
     Array.from(data)
       .filter(option => option.ccy === currency1)
@@ -64,9 +29,8 @@ function Convert() {
       .filter(option => option.ccy === currency1)
       .map(option => option.sale)
   );
-  let rezBuy = money * courseBuy;
-  let rezSale = money * courseSale;
-
+  rezBuy = (money * courseBuy).toFixed(2);
+  rezSale = (money * courseSale).toFixed(2);
   if (currency1 != "BTC")
     label.innerHTML =
       "Buy:  " +
@@ -89,15 +53,13 @@ function Convert() {
       rezSale +
       "  " +
       "USD";
-
-  // let money = document.querySelector(".userPhone").value;
 }
 function Init() {
   let table = document.createElement("table");
   table.setAttribute("class", "table");
   row = document.createElement("tr");
   row.innerHTML =
-    "<th>ID</th><th>User</th><th>Email</th><th>Phone</th><th>Message</th>";
+    "<th>ID</th><th>User</th><th>Email</th><th>Phone</th><th>Message</th><th>Currency</th>";
   table.appendChild(row);
   converter = document.querySelector(".converter");
   converter.appendChild(select_first);
@@ -105,7 +67,10 @@ function Init() {
   label.setAttribute("style", "color: black;margin-left: 15px;");
   inputMoney.setAttribute("type", "number");
   inputMoney.setAttribute("style", "color: black;margin-left: 15px;");
+  select_second.setAttribute("style", "color: black;margin-left: 15px;");
+
   converter.appendChild(inputMoney);
+  // converter.appendChild(select_second);
   converter.appendChild(label);
   contact.appendChild(table);
   let button = document.querySelector(".btnSend");
@@ -131,7 +96,6 @@ function Request() {
           row = document.createElement("tr");
           select_first.options[i] = new Option(data[i].ccy, data[i].ccy);
           select_first.setAttribute("style", "color: black;");
-
           row.innerHTML = `<td>${data[i].ccy}</td><td>${data[i].base_ccy}</td><td>${data[i].buy}</td><td>${data[i].sale}</td>`;
           table.appendChild(row);
         }
@@ -149,6 +113,7 @@ function GetData() {
   let email = document.querySelector(".email").value;
   let userPhone = document.querySelector(".userPhone").value;
   let SendMessage = document.querySelector(".SendMessage").value;
+  let currency = select_first.value;
   let formData = {
     userName: userName,
     email: email,
@@ -157,6 +122,6 @@ function GetData() {
   };
   table = document.querySelector(".table");
   row = document.createElement("tr");
-  row.innerHTML = `<td>${table.rows.length}</td><td>${formData.userName}</td><td>${formData.email}</td><td>${formData.userPhone}</td><td>${formData.SendMessage}</td>`;
+  row.innerHTML = `<td>${table.rows.length}</td><td>${formData.userName}</td><td>${formData.email}</td><td>${formData.userPhone}</td><td>${formData.SendMessage}</td><td>${inputMoney.value} ${select_first.value}/${rezSale}UAH</td>`;
   table.appendChild(row);
 }
